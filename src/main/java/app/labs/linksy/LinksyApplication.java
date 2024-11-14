bfefa7aeb70a90de5fac46bfba53b0fc9c896ce0
재미 🅵🆄🅽 게임 🅶🅰🅼🅴 <83849758+quiencomotu03@users.noreply.github.com>
2024-11-14 15:34:16 +0900

    1st commit

---
+++ src/main/java/app/labs/linksy/LinksyApplication.java
@@ -0,0 +1,13 @@
+package app.labs.linksy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinksyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/labs/linksy/LinksyApplicationTests.java
@@ -0,0 +1,13 @@
+package app.labs.linksy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
