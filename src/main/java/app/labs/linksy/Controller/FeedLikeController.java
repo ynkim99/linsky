dfaea4bb5b7c723950e48d973be30284211742ef
재미 🅵🆄🅽 게임 🅶🅰🅼🅴 <83849758+quiencomotu03@users.noreply.github.com>
2024-11-23 01:50:55 +0900

    LikesList DONE //2024..11.23

--- src/main/java/app/labs/linksy/Controller/FeedLikeController.java
+++ src/main/java/app/labs/linksy/Controller/FeedLikeController.java
@@ -4,12 +4,13 @@ import app.labs.linksy.Model.Member;
 import app.labs.linksy.Service.FeedLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:80")
-@RestController
+@Controller
 @RequestMapping("/api/feed")
 @RequiredArgsConstructor
 public class FeedLikeController {
@@ -25,4 +26,16 @@ public class FeedLikeController {
         }
         return ResponseEntity.ok(profiles);
     }
+
+    @GetMapping("/likeslist")
+    public String showLikesList() {
+        return "likeslist"; // likeslist.html 파일을 반환
+    }
+
+    @GetMapping("/likes")
+    @ResponseBody
+    public List<Member> getLikesList(@RequestParam("feedId") int feedId) {
+        return feedLikeService.getLikeProfilesByFeedId(feedId);
+    }
+
 }
